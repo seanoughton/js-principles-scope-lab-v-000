66e5080f21ace270579eaa4223a95d693631ff2d
Sean Oughton <seanoughton@gmail.com>
2018-06-20 18:47:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
-leastFavoriteCustomer = 'me';
+const leastFavoriteCustomer = 'me';
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
@@ -17,5 +17,5 @@ function overwriteBestCustomer() {
 }
 
 function changeLeastFavoriteCustomer(){
-  
+
 }
